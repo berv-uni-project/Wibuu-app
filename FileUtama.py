046ea8086b9550323cd2ba657e994c615d481c60
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-04-18 23:47:47 +0700

    Bring Comment

--- FileUtama.py
+++ FileUtama.py
@@ -6,7 +6,6 @@ from OpenGL.GLU import *
 window = 0  # glut window number
 width, height = 800, 600  # window size
 
-
 def draw_rect(x, y, width, height):
     glBegin(GL_QUADS)  # start drawing a rectangle
     glVertex2f(x, y)  # bottom left point
@@ -14,7 +13,6 @@ def draw_rect(x, y, width, height):
     glVertex2f(x + width, y + height)  # top right point
     glVertex2f(x, y + height)  # top left point
     glEnd()  # done drawing a rectangle
-
 
 def draw_circle(x, y, rad):
     posx, posy = x, y
@@ -28,7 +26,6 @@ def draw_circle(x, y, rad):
         glVertex2f(cosine, sine)
     glEnd()
 
-
 def draw_circle_custom(x, y, rad, n):
     posx, posy = x, y
     sides = 32
@@ -40,7 +37,6 @@ def draw_circle_custom(x, y, rad, n):
         glVertex2f(cosine, sine)
     glEnd()
 
-
 def refresh2d(w, h):
     glViewport(0, 0, w, h)
     glMatrixMode(GL_PROJECTION)
@@ -48,7 +44,6 @@ def refresh2d(w, h):
     glOrtho(0.0, w, 0.0, h, 0.0, 1.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-
 
 def draw_background():
     glBegin(GL_QUADS)
@@ -62,7 +57,6 @@ def draw_background():
     glEnd()
 
 def colour_tree(x, y, scale):
-
     glBegin(GL_POLYGON)
     glColor3f(0.4, 0.19, 0.05)
 
@@ -96,48 +90,29 @@ def colour_tree(x, y, scale):
     glVertex2f((x + 175) * scale, (y + 250) * scale)
     glEnd()
 
-def draw_tree(x, y):
-    glColor3f(1.0, 1.0, 1.0)
-    glBegin(GL_LINE_LOOP)
-    glVertex2f(x + 100, y + 250)
-    glVertex2f(x + 175, y + 250)
-    glVertex2f(x + 175, y + 75)
-    glVertex2f(x + 100, y + 75)
-    glEnd()
-    glBegin(GL_LINE_STRIP)
-    glVertex2f(x + 100, y + 250)
-    glVertex2f(x + 0, y + 250)
-    glVertex2f(x + 75, y + 350)
-    glVertex2f(x + 25, y + 350)
-    glVertex2f(x + 100, y + 425)
-    glVertex2f(x + 50, y + 425)
-    glVertex2f(x + 140, y + 500)
-    glVertex2f(x + 225, y + 425)
-    glVertex2f(x + 175, y + 425)
-    glVertex2f(x + 250, y + 350)
-    glVertex2f(x + 200, y + 350)
-    glVertex2f(x + 275, y + 250)
-    glVertex2f(x + 175, y + 250)
-    glEnd()
-    colour_tree(x,y, 0.4)
-
 def draw():  # ondraw is called all the time
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # clear the screen
     glLoadIdentity()  # reset position
     refresh2d(width, height)  # set mode to 2d
 
-    # glColor3f(0.0, 0.0, 1.0)  # set color to blue
-    # draw_rect(0, 0, width, height)  # rect at (10, 10) with width 200, height 100
+    # background
     draw_background()
 
+    # Gunung 1
     glColor3f(0.0, 0.6, 0.0)
     draw_circle_custom(200, -100, 300, 18)
+
+    # Gunung 2
     glColor3f(0.0, 0.8, 0.0)
     draw_circle_custom(600, -100, 300, 18)
 
+    # pohon 1
     colour_tree(50, 0, 0.6)
+
+    # pohon 2
     colour_tree(800, 0, 0.7)
 
+    # matahari
     draw_circle(width * 0.5, height * 0.9, 50)  # draw circle at (300,300) with radius 50
 
     glutSwapBuffers()  # important for double buffering
